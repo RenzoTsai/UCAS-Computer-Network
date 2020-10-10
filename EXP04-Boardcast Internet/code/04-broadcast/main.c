cdd5f7b36f545b55e97138b6169944f9052042c9
RenzoTsai <cairunze17@mails.ucas.ac.cn>
2020-10-10 11:06:44 +0800

    Update main.c

--- EXP04-Boardcast Internet/code/04-broadcast/main.c
+++ EXP04-Boardcast Internet/code/04-broadcast/main.c
@@ -42,6 +42,12 @@ void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 {
 	// TODO: broadcast packet 
 	fprintf(stdout, "TODO: broadcast packet here.\n");
+	iface_info_t * iface_entry = NULL;
+	list_for_each_entry(iface_entry, &instance->iface_list, list) {
+		if (iface_entry -> fd != iface -> fd) {
+			iface_send_packet(iface_entry, packet, len);
+		}
+	}
 }
 
 void handle_packet(iface_info_t *iface, char *packet, int len)
