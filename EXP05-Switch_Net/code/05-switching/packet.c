c5c722cc61545d8447586e8d31240bbaf59ca95c
RenzoTsai <cairunze17@mails.ucas.ac.cn>
2020-10-16 13:08:20 +0800

    Update packet.c

--- EXP05-Switch_Net/code/05-switching/packet.c
+++ EXP05-Switch_Net/code/05-switching/packet.c
@@ -34,5 +34,11 @@ void iface_send_packet(iface_info_t *iface, char *packet, int len)
 void broadcast_packet(iface_info_t *iface, char *packet, int len)
 {
 	// TODO: implement the broadcast process here
-	fprintf(stdout, "TODO: implement the broadcast process here.\n");
+	// fprintf(stdout, "TODO: implement the broadcast process here.\n");
+	iface_info_t * iface_entry = NULL;
+	list_for_each_entry(iface_entry, &instance->iface_list, list) {
+		if (iface_entry->fd != iface->fd ) {
+			iface_send_packet(iface_entry, packet, len);
+		}
+	}
 }
